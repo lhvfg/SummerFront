f2c639b71f17ea972aa2d6651d71e8c2482a47c5
lhvfg <1944148429@qq.com>
2023-07-08 21:33:55 +0800

    完成了新建单词的所有功能

--- src/views/addWord.vue
+++ src/views/addWord.vue
@@ -1,13 +1,14 @@
 <script setup>
-import { ref, reactive, watch,onMounted } from 'vue'
+import { ref, reactive, watch, onMounted } from 'vue'
 import { useRouter } from "vue-router";
 import axios from "axios";
 import { useUserStore } from "../stores/User"
 
+const userStore = useUserStore();
 const spell = ref('');
 let meaningList = ref([{
     mean: null,
-    Function: null
+    part: null
 }]);
 let sentences = ref([{
     content: null,
@@ -32,7 +33,28 @@ const validSpell = ref(false);
 var flag;
 let wordId;
 var checkedBook = new Array();
-
+//清空界面
+function clearAll() {
+    spell.value = '';
+    meaningList.value = [{
+        mean: null,
+        part: null
+    }];
+    sentences.value = [{
+        content: null,
+        contentMean: null
+    }];
+    notes.value = [{
+        content: null
+    }]
+    centerDialogVisible.value = false;
+    spellValidationText = "Please enter the word spell"
+    books.value = []
+    dis.value = [];
+    validSpell.value = false;
+    wordId = null;
+    checkedBook = new Array();
+}
 //获取单词书
 onMounted(() => {
     let request = {
@@ -42,6 +64,8 @@ onMounted(() => {
         (res) => {
             console.log(res);
             books.value = res.data.bookList;
+            books.value = books.value.filter((item) => { return item.hide != 1 });
+            console.log(books.value);
         })
 })
 //判断重复
@@ -72,7 +96,7 @@ function checkSpell() {
 }
 //输入数据
 function addMeaning(i) {
-    if (meaningList.value[i].Function == null || meaningList.value[i].mean == null) {
+    if (meaningList.value[i].part == null || meaningList.value[i].mean == null) {
         ElMessage({
             type: "warning",
             message: "当前词条存在null,不可添加新词条！",
@@ -80,7 +104,7 @@ function addMeaning(i) {
         });
     }
     else {
-        meaningList.value.push({ mean: null, Function: null });
+        meaningList.value.push({ mean: null, part: null });
     }
 }
 function deleteMeaningRequest(i, type) {
@@ -94,7 +118,7 @@ function deleteMeaningRequest(i, type) {
     else {
         typeValue = type;
         deletIndex = i;
-        if (meaningList.value[i].Function != null || meaningList.value[i].mean != null) {
+        if (meaningList.value[i].part != null || meaningList.value[i].mean != null) {
             centerDialogVisible.value = true;
         }
         else {
@@ -174,8 +198,7 @@ function Delete() {
         notes.value.splice(deletIndex, 1)
 }
 //选中单词书
-function choose(i,id)
-{
+function choose(i, id) {
     if (dis.value[i]) {
         console.log(id + '号单词书被选中');
         checkedBook.push(id);
@@ -188,11 +211,45 @@ function choose(i,id)
         console.log(checkedBook);
     }
 }
+() => {
+
+}
+//添加单词
 function createWord() {
     let request = {
         requestType: "addWord",
-        pageNumber: 1
+        wordId: wordId,
+        wordType: validSpell.value ? 1 : 0,
+        spell: spell.value,
+        meaningContent: meaningList.value.map(item => item.mean),
+        function: meaningList.value.map(item => item.part),
+        sentenceContent: sentences.value.map(item => item.content),
+        sentenceContentMean: sentences.value.map(item => item.contentMean),
+        noteContent: notes.value.map(item => item.content),
+        userId: userStore.userId,
+        bookId: checkedBook
     };
+    Request.post("http://localhost:8080/addword", request).then(
+        (res) => {
+            console.log(res);
+            if (res.data.status == "addWordSucceed") {
+                ElMessage({
+                    type: "success",
+                    message: "创建成功",
+                    duration: 2000,
+                });
+                clearAll();
+            }
+            else {
+                ElMessage({
+                    type: "error",
+                    message: "出错了！",
+                    duration: 2000,
+                });
+            }
+        }
+    )
+
 }
 
 
@@ -219,7 +276,7 @@ function createWord() {
                     {{ spellValidationText }}
                 </li>
                 <li v-for="(meaning, index) in meaningList">
-                    <input v-model="meaning.Function" type="text" placeholder="词性">
+                    <input v-model="meaning.part" type="text" placeholder="词性">
                     <input v-model="meaning.mean" type="text" placeholder="对应释义">
                     <button type="button" @click="addMeaning(index)">添加</button>
                     <button type="button" @click="deleteMeaningRequest(index, types[0])">删除</button>
@@ -235,8 +292,8 @@ function createWord() {
                     <button type="button" @click="addNote(index)">添加</button>
                     <button type="button" @click="deleteNoteRequest(index, types[2])">删除</button>
                 </li>
-                <li v-for="(book,index) in books">
-                    {{ book.bookName }}<input v-model="dis[index]" type="checkbox" @change="choose(index,book.id)" />
+                <li v-for="(book, index) in books">
+                    {{ book.bookName }}<input v-model="dis[index]" type="checkbox" @change="choose(index, book.id)" />
                 </li>
                 <li>
                     <button type="submit">
@@ -255,6 +312,7 @@ function createWord() {
 .right {
     color: #00c700;
 }
+
 .dialog-footer button:first-child {
     margin-right: 10px;
 }
